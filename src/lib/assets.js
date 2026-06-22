844d548d6fdc3444e6539f1b929e287e2fe20dd3
thakursammershetty <119006657+thakursammershetty@users.noreply.github.com>
2026-06-22 14:35:09 +0530

    chore: update asset Cloudinary URLs and refactor animation loop timing logic

--- src/lib/assets.js
+++ src/lib/assets.js
@@ -1,21 +1,21 @@
 export const ASSETS = {
   "layers": {
-    "fourth": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897460/satyadev_assets/layers/fourth.png",
-    "last": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897464/satyadev_assets/layers/last.png",
-    "second": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897465/satyadev_assets/layers/second.png",
-    "third": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897467/satyadev_assets/layers/third.png",
-    "top": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897469/satyadev_assets/layers/top.png"
+    "fourth": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118954/satyadev_assets/layers/fourth.png",
+    "last": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118959/satyadev_assets/layers/last.png",
+    "second": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118961/satyadev_assets/layers/second.png",
+    "third": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118963/satyadev_assets/layers/third.png",
+    "top": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118966/satyadev_assets/layers/top.png"
   },
   "mobileLayers": {
-    "fourth": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897470/satyadev_assets/mobile-layers/fourth.png",
-    "last": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897471/satyadev_assets/mobile-layers/last.png",
-    "second": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897473/satyadev_assets/mobile-layers/second.png",
-    "third": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897474/satyadev_assets/mobile-layers/third.png",
-    "top": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897475/satyadev_assets/mobile-layers/top.png"
+    "fourth": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118968/satyadev_assets/mobile-layers/fourth.png",
+    "last": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118970/satyadev_assets/mobile-layers/last.png",
+    "second": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118974/satyadev_assets/mobile-layers/second.png",
+    "third": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118975/satyadev_assets/mobile-layers/third.png",
+    "top": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118976/satyadev_assets/mobile-layers/top.png"
   },
   "wallLayers": {
-    "wall-layer-desktop": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897476/satyadev_assets/wall-layers/wall-layer-desktop.png",
-    "wall-layer-mobile": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781899040/satyadev_assets/wall-layers/wall-layer-mobile.png",
-    "wall-layer-tablet": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1781897479/satyadev_assets/wall-layers/wall-layer-tablet.png"
+    "wall-layer-desktop": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118978/satyadev_assets/wall-layers/wall-layer-desktop.png",
+    "wall-layer-mobile": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118979/satyadev_assets/wall-layers/wall-layer-mobile.png",
+    "wall-layer-tablet": "https://res.cloudinary.com/dbn2ye2zo/image/upload/f_auto,q_auto/v1782118981/satyadev_assets/wall-layers/wall-layer-tablet.png"
   }
 };
